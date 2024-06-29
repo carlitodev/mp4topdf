789f49b411aff84aeb87b753697bdf94f0074a00
Carlito <rickdudek.z@gmail.com>
2024-06-29 19:28:15 -0300

    Fix port

--- app.py
+++ app.py
@@ -79,5 +79,6 @@ def convert(filename):
 
     return render_template('convert.html', filename=filename)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
